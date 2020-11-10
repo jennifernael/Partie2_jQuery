251153a5b8d2c558411a703e636edcc18627f4b8
jennifernael <jennifer.nael@hotmail.fr>
2020-11-10 09:21:21 +0100

    ex 3

---
+++ Exercice 3/assets/js/script3.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    // = $(function(){
+  
+    //Dès qu'on clique sur #hide, on applique hide() au text
+    $("#hide").click(function(){
+        $("#text").hide();
+
+    //Dès qu'on clique sur #show, on applique show() au text
+    $("#show").click(function(){
+        $("#text").show();
+
+        
+    });
+    });     
+  });
