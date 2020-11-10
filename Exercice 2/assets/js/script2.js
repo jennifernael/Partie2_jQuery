6340098be48d4b47f643fd949c936ead370ce3c0
jennifernael <jennifer.nael@hotmail.fr>
2020-11-10 09:20:28 +0100

    ex 2

---
+++ Exercice 2/assets/js/script2.js
@@ -0,0 +1,8 @@
+// le code jQuery sera lu au final de chargement de la page grâce à cette formule
+$(document).ready(function(){
+  // = $(function(){
+
+    $("#button").click(function(){
+      alert("Vous avez cliqué !");
+    });
+});
